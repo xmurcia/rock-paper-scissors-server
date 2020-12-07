096fd63c7f164a971646b14762be65ef12761dcc
Xavier Murcia <xavier.murcia@wefoxgroup.com>
2020-12-07 19:41:08 +0100

    Handle IOManager initialization

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,23 @@
-import express from 'express';
+import IOManager from './io';
+import app from "./app";
+import http from 'http';
+import SocketIO from 'socket.io';
 
+/**
+ * Start Express server.
+ */
+const server = http.createServer(app);
+server.listen(app.get('port'));
 
-const app = express();
+/**
+ * Start SocketIO.
+ */
+const options = {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+};
+
+IOManager.init(new SocketIO.Server(server, options));
+
